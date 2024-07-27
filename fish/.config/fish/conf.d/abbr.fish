32b1dc17f69aca05b1f0770ea255edbedf104b8a
jnkrm <jo.nykrem@mf.me>
2024-07-28 00:00:24 +0200

    second commit

---
+++ fish/.config/fish/conf.d/abbr.fish
@@ -0,0 +1,58 @@
+# Abbreviations
+
+# Open in VS Code
+abbr cfs "code ~/.config/fish/config.fish"
+abbr cab "code ~/.config/fish/conf.d/abbr.fish"
+abbr cal "code ~/.config/alacritty/alacritty.toml"
+
+# Homebrew
+abbr bi "brew install"
+abbr bic "brew install --no-quarantine --cask"
+abbr bcl "brew cleanup"
+abbr bl "brew list"
+abbr bs "brew search"
+abbr brm "brew remove"
+abbr bu "brew update && brew upgrade"
+# abbr brow "/usr/local/Homebrew/bin/brew"
+
+# Buku
+abbr b "buku"
+
+# Restart fish shell
+abbr ef "exec fish"
+
+# Force remove files and directories
+abbr rmr "rm -rf"
+
+# Clear
+abbr cl "clear"
+
+# Mathematica
+abbr maths "MathKernel -script"
+
+# Kira
+# abbr kira "/usr/local/bin/kira"
+# abbr rmk "rm -rf kira.log firefly.log ff_save firefly_saves tmp sectormappings results"
+
+# Login credentials to SSH into Debian server on XPS13
+abbr xps13 "ssh jnkrm@192.168.1.21"
+
+# rsync
+abbr rs "rsync -rvza --rsh='ssh -p1998'"
+
+# eza
+abbr la "eza --long --all --header --icons"
+abbr lt "eza --long --all --header --icons --tree --level=2"
+
+# z
+# abbr cd "z"
+
+# zellij
+abbr zj "zellij attach || zellij"
+
+# web-search plugin
+abbr ws "web-search"
+abbr ddg --position anywhere "duckduckgo"
+
+# Path to the iCloud directory
+abbr ic --position anywhere "/Users/jnkrm/Library/Mobile\ Documents/com~apple~CloudDocs"
